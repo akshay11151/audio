60862ad441fb517627c7b97c78f4653df3f83aa1
AKSHAY <73608800+akshay11151@users.noreply.github.com>
2023-04-25 17:19:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
-import io
+import speech_recognition as sr
 import os
+import datetime
+import requests
 
-from google.cloud import speech_v1p1beta1 as speech
+def transcribe_audio(file_path):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(file_path) as source:
+        audio_text = recognizer.recognize_google(source)
+    return audio_text.lower()
 
+def categorize_text(audio_text):
+    if 'idea' in audio_text.split() or 'suggestion' in audio_text.split():
+        return 'ideas'
+    elif 'hate' in audio_text.split() or 'anger' in audio_text.split() or 'illogical' in audio_text.split() or 'simping' in audio_text.split():
+        return 'behavioral improvements'
+    else:
+        return 'casual talk'
 
-def transcribe_file_with_auto_language(file_path):
-    """Transcribe the given audio file using Google Cloud Speech-to-Text API with auto language detection."""
-    client = speech.SpeechClient()
+def upload_file_to_drive(file_path):
+    upload_url = 'https://www.example.com/upload'
+    with open(file_path, 'rb') as f:
+        r = requests.post(upload_url, files={'file': f})
+    if r.status_code == 200:
+        print('File uploaded successfully')
+    else:
+        print('Failed to upload file')
 
-    with io.open(file_path, "rb") as audio_file:
-        content = audio_file.read()
+def main():
+    while True:
+        time_now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        file_path = f'{time_now}.wav'
+        print(f'Recording started at {time_now}')
+        os.system(f'rec -c 1 -r 16000 {file_path} silence 1 0.1 1% 1 2.0 1%')
+        print(f'Recording stopped at {datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
+        audio_text = transcribe_audio(file_path)
+        print(f'Transcribed text: {audio_text}')
+        category = categorize_text(audio_text)
+        print(f'Category: {category}')
+        if category != 'casual talk':
+            upload_file_to_drive(file_path)
+        os.remove(file_path)
 
-    audio = speech.RecognitionAudio(content=content)
-
-    config = speech.RecognitionConfig(
-        encoding="LINEAR16",
-        sample_rate_hertz=44100,
-        language_code="",
-        enable_automatic_punctuation=True,
-        use_enhanced=True,
-        model="default",
-    )
-
-    response = client.recognize(config=config, audio=audio)
-
-    for result in response.results:
-        print(f"Transcript: {result.alternatives[0].transcript}")
-
-
-if __name__ == "__main__":
-    # Set the path to your service account key JSON file
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "D:/Downs/audio.json"
-
-    # Set the path to your audio file
-    file_path = "D:/Downs/A_J_Cook_Speech_from_Lansbury's_Labour_Weekly.ogg"
-
-    # Transcribe the audio file
-    transcribe_file_with_auto_language(file_path)
+if __name__ == '__main__':
+    main()
